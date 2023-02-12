b2a8e2c0f29b957e6114616c988f48196e248425
nuwan <techiprimers@gmail.com>
2023-02-12 21:21:58 +0530

    test api

---
+++ src/main/java/com/web/webproject/controller/AdministrationController.java
@@ -0,0 +1,9 @@
+package com.web.webproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "administration")
+public class AdministrationController {
+}
---
+++ src/main/java/com/web/webproject/controller/EmployeeController.java
@@ -0,0 +1,20 @@
+package com.web.webproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "employee")
+public class EmployeeController {
+
+    @GetMapping(value = "getEmployeeDetails")
+    public void getEmployeeDetails() {
+        System.out.println("Hello I will send all the employee Details");
+        System.out.println(EmployeeController.calculateSum(12,34));
+    }
+
+    private static Integer calculateSum(int valu1, int valu2) {
+        return valu1 + valu2;
+    }
+}
